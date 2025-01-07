4957a45d336367e0e3d6da3a4c448ccf4235e759
Calco Dev <calcopoddev@gmail.com>
2025-01-08 00:12:10 +0200

    feat: kinda raytracing sth?

--- scenes/_debug/particle_sim_demo/particle.glsl
+++ scenes/_debug/particle_sim_demo/particle.glsl
@@ -3,7 +3,46 @@
 
 layout(rgba32f, binding = 0, set = 0) uniform image2D img;
 
+layout(push_constant, std430) uniform Params {
+    mat4 _camera_to_world;
+    mat4 _camera_inverse_proj;
+} p;
+
+struct Ray {
+    vec3 origin;
+    vec3 direction;
+};
+
+Ray create_ray(vec3 origin, vec3 direction) {
+    Ray ray;
+    ray.origin = origin;
+    ray.direction = direction;
+    return ray;
+}
+
+Ray create_camera_ray(vec2 uv) {
+    // Transform the camera origin to world space
+    vec3 origin = (p._camera_to_world * vec4(0.0, 0.0, 0.0, 1.0)).xyz;
+
+    // Invert the perspective projection of the view-space position
+    vec3 direction = (p._camera_inverse_proj * vec4(uv, 0.0, 1.0)).xyz;
+    // Transform the direction from camera to world space and normalize
+    direction = (p._camera_to_world * vec4(direction, 0.0)).xyz;
+    direction = normalize(direction);
+    return create_ray(origin, direction);
+}
+
 layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 void main() {
-    imageStore(img, ivec2(gl_GlobalInvocationID.xy), vec4(1.0, 0.0, 0.0, 1.0));
+    ivec2 pixel_pos = ivec2(gl_GlobalInvocationID.xy);
+
+    ivec2 image_size = imageSize(img);
+    vec2 uv = ((gl_GlobalInvocationID.xy) / vec2(image_size) * 2.0 - 1.0);
+    float ar = float(image_size.x) / float(image_size.y);
+    uv.x *= ar;
+
+    Ray ray = create_camera_ray(uv);
+
+    imageStore(img, pixel_pos, vec4(ray.direction * 0.5 + 0.5, 1.0));
+    // imageStore(img, ivec2(gl_GlobalInvocationID.xy), vec4(1.0, 0.0, 0.0, 1.0));
 }
--- scenes/_debug/particle_sim_demo/particle_simulation.gd
+++ scenes/_debug/particle_sim_demo/particle_simulation.gd
@@ -4,6 +4,8 @@ extends Node2D
 @export var tex_size := Vector2i(320, 240)
 @export var _output_sprite: Sprite2D
 
+@export var camera: Camera3D
+
 #region Node Lifecycle
 func _ready() -> void:
 	var img := Image.create(tex_size.x, tex_size.y, false, Image.FORMAT_RGBAF)
@@ -13,16 +15,20 @@ func _ready() -> void:
 	_thread_invoke(_init_compute)
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("jump"):
-		if _thread_invoke(_render_compute):
-			_rd.submit()
-			_rd. sync ()
-			var data := _rd.texture_get_data(_tex_rid, 0)
-			var img := Image.create_from_data(tex_size.x, tex_size.y, false, Image.FORMAT_RGBAF, data)
-			(_output_sprite.texture as ImageTexture).update(img)
+	# if Input.is_action_just_pressed("jump"):
+	_do_render()
 
 func _exit_tree() -> void:
 	_thread_invoke(_free_compute)
+
+func _do_render() -> void:
+	if _thread_invoke(_render_compute):
+		_rd.submit()
+		_rd.sync()
+		var data := _rd.texture_get_data(_tex_rid, 0)
+		var img := Image.create_from_data(tex_size.x, tex_size.y, false, Image.FORMAT_RGBAF, data)
+		(_output_sprite.texture as ImageTexture).update(img)
+
 #endregion
 
 #region Compute Lifecycle
@@ -61,13 +67,26 @@ func _init_compute() -> void:
 	_tex_rid = _rd.texture_create(tf, RDTextureView.new(), [])
 	_rd.texture_clear(_tex_rid, Color.BLACK, 0, 1, 0, 1)
 
-	var uniform := RDUniform.new()
-	uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
-	uniform.binding = 0
-	uniform.add_id(_tex_rid)
-	_tex_uniform_set_rid = _rd.uniform_set_create([uniform], _shader_rid, 0)
+	# texture uniform
+	var tex_uniform := RDUniform.new()
+	tex_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
+	tex_uniform.binding = 0
+	tex_uniform.add_id(_tex_rid)
+
+	# uniform set
+	_tex_uniform_set_rid = _rd.uniform_set_create([tex_uniform], _shader_rid, 0)
 
 	_pipeline_rid = _rd.compute_pipeline_create(_shader_rid)
+
+func matrix_to_bytes(t: Transform3D) -> PackedFloat32Array:
+	var basis := t.basis
+	var origin := t.origin
+	return PackedFloat32Array([
+		basis.x.x, basis.x.y, basis.x.z, 1.0,
+		basis.y.x, basis.y.y, basis.y.z, 1.0,
+		basis.z.x, basis.z.y, basis.z.z, 1.0,
+		origin.x, origin.y, origin.z, 1.0
+	])
 
 func _render_compute() -> void:
 	@warning_ignore("INTEGER_DIVISION")
@@ -75,9 +94,18 @@ func _render_compute() -> void:
 	@warning_ignore("INTEGER_DIVISION")
 	var y_groups := (tex_size.y - 1) / 8 + 1
 
+	# push constant data
+	var pc := PackedFloat32Array()
+	pc.append_array(matrix_to_bytes(camera.global_transform))
+	var proj := camera.get_camera_projection()
+	for i in 4:
+		for j in 4:
+			pc.append(proj[i][j])
+
 	var cl_rid := _rd.compute_list_begin()
 	_rd.compute_list_bind_compute_pipeline(cl_rid, _pipeline_rid)
 	_rd.compute_list_bind_uniform_set(cl_rid, _tex_uniform_set_rid, 0)
+	_rd.compute_list_set_push_constant(cl_rid, pc.to_byte_array(), pc.size() * 4)
 	_rd.compute_list_dispatch(cl_rid, x_groups, y_groups, 1)
 	_rd.compute_list_end()
 
